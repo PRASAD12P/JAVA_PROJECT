452d55513fc60a02ac7f5d5f9466e5fd17839b3a
Prasad <mdbprasad223@gmail.com>
2024-10-04 22:13:22 +0530

    Initialfile

---
+++ Menu.java
@@ -0,0 +1,13 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Menu{
+         public static void main(String[] args) {
+            JFrame f = new JFrame("Menu");
+            f.setSize(500,600);
+            f.setVisible(true);
+         }
+} 
